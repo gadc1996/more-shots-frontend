a2bc7874718bc8a1891af6f7ff095ab619645cb7
gdelgado <gadc1996@gmail.com>
2021-10-19 07:19:51 -0500

    feat(): logout user

--- src/store/modules/Login.js
+++ src/store/modules/Login.js
@@ -22,6 +22,7 @@ const actions = {
       const response = await axios.post('http://more-shots.test/api/login', payload)
         commit('SET_USER', response.data.data)
       } catch (error){
+        commit('SET_USER', {})
         console.log(error.response.statusText)
       }
   }
